53f9746abca727e60a5f2dce05292b72466f58fc
islo123 <43340489+islo123@users.noreply.github.com>
2022-10-31 10:03:02 +0200

    cors

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,6 +2,21 @@ const User = require('../models/User')
 const { StatusCodes } = require('http-status-codes')
 
 const register = async (req, res) => {
+   // Website you wish to allow to connect
+   res.setHeader('Access-Control-Allow-Origin', 'https://employee-information-unique.netlify.app');
+
+   // Request methods you wish to allow
+   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+   // Request headers you wish to allow
+   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+   // Set to true if you need the website to include cookies in the requests sent
+   // to the API (e.g. in case you use sessions)
+   res.setHeader('Access-Control-Allow-Credentials', true);
+
+   // Pass to next layer of middleware
+   next();
   const user = await User.create({ ...req.body })
   const token = user.createJWT()
   res.status(StatusCodes.CREATED).json({ user: { name: user.name }, token })
