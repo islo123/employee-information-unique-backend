2711a75f964c27db14ca60fa6f6049b3dc4cd359
islo123 <43340489+islo123@users.noreply.github.com>
2022-11-03 15:03:28 +0200

    exsits email

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,6 +2,11 @@ const User = require('../models/User')
 const { StatusCodes } = require('http-status-codes')
 
 const register = async (req, res) => {
+  const { email } = req.body
+  const exists = await User.findOne({ email })
+  if(exists) {
+    return res.sendStatus(409)
+  }
   const user = await User.create({ ...req.body })
   const token = user.createJWT()
   res.status(StatusCodes.CREATED).json({ user: { name: user.name }, token })
